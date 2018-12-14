ab54e58e3e75a09e98193ef6c8199efcb8ed87e9
holland cookson <hollandcookson@hollands-MacBook-Pro-2.local>
2018-12-14 10:40:19 -0500

    testing NYT API functionality

---
+++ nyt-api/nyt-ajax.js
@@ -0,0 +1,50 @@
+
+console.log("hello")
+
+var axios = require('axios');
+
+var APIKey = 'fc95978666c547148b6c203afe174c81';
+
+// module.exports = function runQuery(search, start, end, qty) {
+module.exports = function nytAPI(data) {
+    console.log("this is the data passed into api()")
+    console.log(data);
+    var search = data.search;
+    var start = data.start;
+    var end = data.end;
+    var numberOfArticles = parseInt(data.qty);
+
+
+    return axios.get('https://api.nytimes.com/svc/search/v2/articlesearch.json', {
+
+        params: {
+            'api-key': APIKey,
+            'q': search,
+            'begin_date': start,
+            'end_date': end,
+        }
+    })
+
+        .then(function (results) {
+            var baseResponse = results.data.response.docs;
+            var returnData = [];
+
+            for (var i = 0; i < numberOfArticles; i++) {
+
+                var article = {
+                    headline: baseResponse[i].headline.main,
+                    snippet: baseResponse[i].snippet,
+                    url: baseResponse[i].web_url,
+                    pub_date: baseResponse[i].pub_date,
+                    source: baseResponse[i].source,
+                    articleId: baseResponse[i]._id,
+                    image: baseResponse[i].multimedia[0].url,
+                }
+
+                returnData.push(article);
+
+            
+            }
+            return returnData;
+        });
+};
